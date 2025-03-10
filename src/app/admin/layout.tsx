d58129b61a574d5ed020e2a392355e250ffc7799
Jack Clackett <jack@noquarter.co>
2025-03-10 18:51:15 +0100

    clean

--- src/app/admin/layout.tsx
+++
@@ -1,7 +0,0 @@
-import { getAdmin } from "@/lib/server/auth"
-import type * as React from "react"
-
-export default async function Layout({ children }: { children: React.ReactNode }) {
-  await getAdmin()
-  return <>{children}</>
-}
